1dc106b0d73cb5f4b9d2fa3d292312eda6a58cb1
ammarahmedbajwa <ammarahmed89754@gmail.com>
2023-01-22 22:00:42 +0500

    script for queue and execute a proposal after voting

---
+++ scripts/queue-and-execute.ts
@@ -0,0 +1,49 @@
+// @ts-ignore
+import { ethers, network } from "hardhat"
+import {
+  FUNC,
+  PROPOSAL_DESCRIPTION,
+  MIN_DELAY,
+  developmentChains,
+} from "../helper-hardhat-config"
+import { moveBlocks } from "../utils/move-blocks"
+import { moveTime } from "../utils/move-time"
+
+export async function queueAndExecute() {
+  const args = []
+  const functionToCall = FUNC
+  const campaign = await ethers.getContract("Campaign")
+  const encodedFunctionCall = campaign.interface.encodeFunctionData(functionToCall, args)
+  const descriptionHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(PROPOSAL_DESCRIPTION))
+  // could also use ethers.utils.id(PROPOSAL_DESCRIPTION)
+
+  const governor = await ethers.getContract("GovernorContract")
+  console.log("Queueing...")
+  const queueTx = await governor.queue([campaign.address], [0], [encodedFunctionCall], descriptionHash)
+  await queueTx.wait(1)
+
+  if (developmentChains.includes(network.name)) {
+    await moveTime(MIN_DELAY + 1)
+    await moveBlocks(1)
+  }
+
+  console.log("Executing...")
+  // this will fail on a testnet because you need to wait for the MIN_DELAY!
+  const executeTx = await governor.execute(
+    [campaign.address],
+    [0],
+    [encodedFunctionCall],
+    descriptionHash
+  )
+  await executeTx.wait(1)
+  const balance = await campaign.getBalance();
+  console.log(`balance is ${balance}`);
+  
+}
+
+queueAndExecute()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
