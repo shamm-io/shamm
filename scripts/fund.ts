d8e383efc50446511f1780a80e74b4ecdcee48f1
ammarahmedbajwa <ammarahmed89754@gmail.com>
2023-02-06 21:26:17 +0500

    nothing much has changed

--- scripts/fund.ts
+++ scripts/fund.ts
@@ -8,17 +8,21 @@ async function fund(ethAmount: string) {
   const { deployer } = await getNamedAccounts()
   const campaign = await ethers.getContract("Campaign", deployer);
   console.log(`Got contract campaign at ${campaign.address}`);
+  const governortoken = await ethers.getContract("GovernanceToken", deployer);
+  console.log(`Got contract governortoken at ${governortoken.address}`);
   const balance = await campaign.getBalance();
   console.log(`balance is ${balance}`);
   console.log("Funding contract...");
   // const transactionResponse = await campaign.connect(addr1).acceptFunding({
   //   value: ethers.utils.parseEther(ethAmount),
   // });
-  const transactionResponse = await campaign.acceptFunding({
+  const transactionResponse = await campaign.requestFunding({
     value: ethers.utils.parseEther(ethAmount),
   });
   await transactionResponse.wait();
-  
+  // if(transactionResponse == true){
+  //   const tx = await governortoken._transfer(5);
+  // }
   
   const governor = await ethers.getContract("GovernorContract", deployer);
   // const transactionResponse1 = await governor.connect(addr1).addCont();
