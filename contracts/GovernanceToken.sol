0300683ae01bac3c4bd6d6c0264fcccac333096f
ammarahmedbajwa <ammarahmed89754@gmail.com>
2023-02-06 21:16:11 +0500

    Now the tokens can be transferred to other addresses

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -5,15 +5,22 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract GovernanceToken is ERC20Votes {
     uint256 public s_maxSupply = 1000000000000000000000000;
+    address public owner;
 
     constructor()
         ERC20("GovernanceToken", "GT")
         ERC20Permit("GovernanceToken")
     {
         _mint(msg.sender, s_maxSupply);
+        owner = msg.sender;
     }
 
     // The functions below are overrides required by Solidity.
+
+    function _transfer(uint256 amount) public {
+        require(msg.sender == owner, "Only owner can transfer");
+        super._transfer(owner, msg.sender, amount);
+    }
 
     function _afterTokenTransfer(
         address from,
@@ -32,5 +39,15 @@ contract GovernanceToken is ERC20Votes {
         uint256 amount
     ) internal override(ERC20Votes) {
         super._burn(account, amount);
+    }
+
+    // work to be done: transfer tokens to the funder upon funding
+    // function inscreaseMaxSupply(uint256 amount) public {
+    //     require(msg.sender == owner, "Only Owner can increase max supply");
+    //     s_maxSupply += amount;
+    // }
+
+    function getOwner() public view returns (address) {
+        return owner;
     }
 }
