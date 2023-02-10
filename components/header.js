eee15e7b8aa684d6d37e5bbfe6f0eef486fad5ef
shahiqzaidi <shahiqzaidi@protonmail.com>
2023-02-10 15:55:39 +0500

    Minor styling tweaks and fund input validation

--- components/header.js
+++ components/header.js
@@ -2,10 +2,20 @@ import { ConnectButton } from "web3uikit";
 
 export default function Header() {
   return (
-    <div className="p-5 border-b-2 flex flex-row">
-      <div className="ml-auto py-2 px-4">
-        <ConnectButton moralisAuth={false} />
-      </div>
+    <div className="container mx-auto px-4 text-center w-full">
+      <nav className="h-20 flex items-center">
+        <div className="w-full flex items-center gap-x-7 transition-colors text-sm font-medium text-dim-gray">
+          <a href="#" className="hover:text-black">Hello</a>
+          <a href="#" className="hover:text-black">World</a>
+          <a href="#" className="hover:text-black">Please</a>
+          <a href="#" className="hover:text-black">Fund</a>
+        </div>
+        <div className="flex items-center justify-end gap-x-2">
+          <div id="connectWalletButton">
+            <ConnectButton moralisAuth={false} />
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
