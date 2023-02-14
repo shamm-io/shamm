4e707b339572466c5411cd9ee25e76d9a6d66019
ammarahmedbajwa <ammarahmed89754@gmail.com>
2023-02-14 22:01:49 +0500

    GET and POST API for proposals.json

--- pages/api/proposals/index.ts
+++ pages/api/proposals/index.ts
@@ -1,18 +1,20 @@
-import { proposalsFile } from '../../../../shamm_2/helper-hardhat-config';
+// import { proposalsFile } from '../../../constants/proposals.json';
 import { useMoralis } from "react-moralis";
 import * as fs from "fs";
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res:NextApiResponse) {
-  // const proposalsFile = "../../shamm_2/proposals.json";
+  const proposalsFile = 'proposals.json';
+  let proposals: any;
   //   const { chainId: chainIdHex } = useMoralis();
   //   const chainId = parseInt(chainIdHex);
-
+  
   if (req.method === "GET") {
-    res.status(200).json(proposalsFile);
+    proposals = JSON.parse(fs.readFileSync(proposalsFile, "utf8"));
+    res.status(200).json(proposals);
   } else if (req.method === "POST") {
-    var chainId = '31337';
-    let proposals: any;
+    var chainId = "31337";
+    
     const proposal = req.body;
     const newProposal = proposal;
     console.log(newProposal);
@@ -26,4 +28,8 @@ export default function handler(req: NextApiRequest, res:NextApiResponse) {
     fs.writeFileSync(proposalsFile, JSON.stringify(proposals), "utf8");
     res.status(201).json(newProposal);
   }
+
+  // const data = req.body;
+  // fs.writeFileSync('../../../proposals.json', JSON.stringify(data), 'utf-8');
+  // res.status(201).send(data);
 }
