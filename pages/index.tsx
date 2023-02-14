2467364b2ee45326a5e84514ec9448af23524ba2
ammarahmedbajwa <ammarahmed89754@gmail.com>
2023-02-14 22:06:46 +0500

    minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,12 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from '../components/header'
 import { useMoralis } from 'react-moralis'
-import Fund from '../components/fund'
+import RequestFunding from '../components/request-funding'
 import Propose from '../components/propose'
 import Vote from '../components/vote'
 import Queue from '../components/queue'
 import Execute from '../components/execute'
+import AcceptFunding from '../components/accept-funding'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,11 +23,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      <Fund/>
+      <RequestFunding/>
+      <AcceptFunding/>
       <Propose/>
       <Vote/>
       <Queue/>
       <Execute/>
+      
 
       
       
